3a6057a4d07b7be059a082a65ee55e5ddccebf37
Gonçalo Marques <goncalodinismarques@gmail.com>
2019-02-18 10:31:24 +0000

    Upgrade slick version to 3.3.0 for scala 2.11 and 2.12

--- project/Build.scala
+++ project/Build.scala
@@ -175,6 +175,6 @@ object Build extends Build {
   def getSlickDependency(slickComponent: String, version: String): ModuleID = {
     "com.typesafe.slick" %
     (slickComponent + "_" + version.substring(0, version.lastIndexOf('.'))) %
-    (if(version.startsWith("2.10")) {"3.1.1"} else {"3.2.3"})
+    (if(version.startsWith("2.10")) {"3.1.1"} else {"3.3.0"})
   }
 }
